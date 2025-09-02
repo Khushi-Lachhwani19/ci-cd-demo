4282d24b7e6a412da965c090e4a2d61c78d1a775
evolution <evolution@evolutions-Mac-mini-2.local>
2025-09-02 12:20:52 +0530

    fix: add eslint and configuration

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-native'],
+  env: {
+    'react-native/react-native': true,
+  },
+  rules: {
+    // Add any custom rules here
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
